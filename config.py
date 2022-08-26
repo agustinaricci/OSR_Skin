76b07e1e2fe974bdde05a2ddac2e5bc1e2af9e79
maria.ricci <maria.ricci@hospitalitaliano.org.ar>
2022-08-26 13:36:28 -0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+import os, os.path as osp
+
+data_dir = 'data/'
+kather2016_root = osp.join(data_dir, 'kather2016/')
+kather100k_root = osp.join(data_dir, 'kather100k/')
+fitzpatrick17k_root = osp.join(data_dir, 'F17K/')
